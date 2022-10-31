7c0d5c74d7dfb68542727944e3dd597ce9fe76f8
Miguel Alves <miguel.alves@outsystems.com>
2022-10-31 18:01:42 +0000

    Added dialog issue example

--- Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
+++ Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Layout;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform;
 
 namespace Avalonia.Boilerplate {
     public class MainWindow : Window {
@@ -9,7 +11,36 @@ namespace Avalonia.Boilerplate {
 #if DEBUG
             this.AttachDevTools();
 #endif
+            
+            this.FindControl<Button>("Dialog").Click += delegate
+            {
+                var window = CreateSampleWindow();
+                window.Height = 200;
+                window.Show(GetWindow());
+            };
         }
+        
+        
+        private Window CreateSampleWindow() {
+            var window = new Window
+            {
+                Height = 200,
+                Width = 200,
+                Content = new StackPanel
+                {
+                    Spacing = 4,
+                    Children =
+                    {
+                        new TextBlock { Text = "Hello world!" }
+                    }
+                },
+                WindowStartupLocation = WindowStartupLocation.CenterOwner
+            };
+
+            return window;
+        }
+        
+        Window GetWindow() => (Window)this.VisualRoot;
 
         private void InitializeComponent() {
             AvaloniaXamlLoader.Load(this);
--- Avalonia.Boilerplate/Avalonia.Boilerplate/Program.cs
+++ Avalonia.Boilerplate/Avalonia.Boilerplate/Program.cs
@@ -2,19 +2,25 @@ using System;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Interactivity;
+using Avalonia.Threading;
+using AppLifetime = Avalonia.Controls.ApplicationLifetimes.ClassicDesktopStyleApplicationLifetime;
 
 namespace Avalonia.Boilerplate {
     class Program {
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
-                .LogToTrace();
+                .With(new Win32PlatformOptions
+                {
+                    UseWindowsUIComposition = false
+                });
+        //.LogToTrace();
     }
 }
