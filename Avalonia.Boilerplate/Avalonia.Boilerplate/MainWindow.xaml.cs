2cddc8a7d81ff48d2a8986865ec14d29f43c00c7
Miguel Alves <alvesmiguel1@users.noreply.github.com>
2022-12-14 14:50:21 +0000

    Added titlebar

--- Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
+++ Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
@@ -3,13 +3,19 @@ using Avalonia.Controls;
 using Avalonia.Layout;
 using Avalonia.Markup.Xaml;
 using Avalonia.Platform;
+using System;
+using System.Reactive.Linq;
+using System.Runtime.InteropServices;
 
 namespace Avalonia.Boilerplate {
     public class MainWindow : Window {
+        private readonly IDisposable windowDecorationMarginSubscription;
+
         public MainWindow() {
+            windowDecorationMarginSubscription = this.GetObservable(WindowDecorationMarginProperty).Subscribe(decoration => ExtendClientAreaTitleBarHeightHint = 234);
             InitializeComponent();
 #if DEBUG
-            this.AttachDevTools();
+            this.AttachDevTools(new Input.KeyGesture(Input.Key.F8));
 #endif
             
             this.FindControl<Button>("Dialog").Click += delegate
@@ -44,6 +50,65 @@ namespace Avalonia.Boilerplate {
 
         private void InitializeComponent() {
             AvaloniaXamlLoader.Load(this);
+            RefreshExtendClientAreaChromeHints();
+            PseudoClasses.Set(":win", RuntimeInformation.IsOSPlatform(OSPlatform.Windows));
+        }
+
+        protected void RefreshExtendClientAreaChromeHints(bool isTitleBarVisible = true)
+        {
+            ExtendClientAreaChromeHints = isTitleBarVisible
+                ? ExtendClientAreaChromeHints.PreferSystemChrome
+                : ExtendClientAreaChromeHints.NoChrome;
+        }
+
+        public static readonly StyledProperty<bool> EnableCloseProperty =
+            AvaloniaProperty.Register<MainWindow, bool>(nameof(EnableClose), defaultValue: true, inherits: true);
+
+        public bool EnableClose
+        {
+            get => GetValue(EnableCloseProperty);
+            set => SetValue(EnableCloseProperty, value);
+        }
+
+        public static readonly StyledProperty<bool> AllowMinimizeProperty =
+           AvaloniaProperty.Register<MainWindow, bool>(nameof(AllowMinimize), defaultValue: true, inherits: true);
+
+        public bool AllowMinimize
+        {
+            get => GetValue(AllowMinimizeProperty);
+            set => SetValue(AllowMinimizeProperty, value);
+        }
+
+        private static bool CanRestoreOrMaximize(IAvaloniaObject window, bool allowRestoreOrMaximize)
+        {
+            return allowRestoreOrMaximize && ((MainWindow)window).CanResize;
+        }
+
+        public static readonly StyledProperty<bool> AllowRestoreOrMaximizeProperty =
+           AvaloniaProperty.Register<MainWindow, bool>(nameof(AllowRestoreOrMaximize), defaultValue: true, inherits: true, coerce: CanRestoreOrMaximize);
+
+        public bool AllowRestoreOrMaximize
+        {
+            get => GetValue(AllowRestoreOrMaximizeProperty);
+            set => SetValue(AllowRestoreOrMaximizeProperty, value);
+        }
+
+        public static readonly StyledProperty<bool> ShowCloseProperty =
+           AvaloniaProperty.Register<MainWindow, bool>(nameof(ShowClose), defaultValue: true, inherits: true);
+
+        public bool ShowClose
+        {
+            get => GetValue(ShowCloseProperty);
+            set => SetValue(ShowCloseProperty, value);
+        }
+
+        public static readonly StyledProperty<bool> ShowPopInProperty =
+           AvaloniaProperty.Register<MainWindow, bool>(nameof(ShowPopIn), defaultValue: false, inherits: true);
+
+        public bool ShowPopIn
+        {
+            get => GetValue(ShowPopInProperty);
+            set => SetValue(ShowPopInProperty, value);
         }
     }
 }
